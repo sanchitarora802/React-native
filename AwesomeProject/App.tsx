28f20d86b0ab0b4d658941788d401ff918531d12
sanchitarora802 <sanchit.arora802@gmail.com>
2023-03-10 18:50:32 +0530

    1. stylesheed added. 2. Notes file created

--- AwesomeProject/App.tsx
+++ AwesomeProject/App.tsx
@@ -1,20 +1,46 @@
 import React from 'react';
-import {View, Button, SafeAreaView, Text, Alert} from 'react-native';
+import {
+  View,
+  Button,
+  Text,
+  Alert,
+  StyleSheet,
+  useColorScheme,
+} from 'react-native';
 
 function App() {
+  const colorScheme = useColorScheme() === 'light';
+
   return (
     <>
-      <SafeAreaView>
-        <View>
-          <Text>Hello World</Text>
-        </View>
-      </SafeAreaView>
-      <Button
-        title="Submit"
-        onPress={() => Alert.alert('Simple Button pressed')}
-      />
+      <View style={styles.wrap}>
+        <Text style={colorScheme ? styles.darkColor : styles.lightColor}>
+          Hello World
+        </Text>
+        <Button
+          title="Submit"
+          onPress={() => Alert.alert('Simple Button pressed')}
+        />
+      </View>
     </>
   );
 }
+
+// Css in key value pair
+const styles = StyleSheet.create({
+  wrap: {
+    // display: 'flex',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingBottom: 10
+  },
+  darkColor: {
+    color: 'black',
+  },
+  lightColor: {
+    color: '#ffffff',
+  },
+});
 
 export default App;
